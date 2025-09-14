d5d447f47d89aca48ea28c141c507e9a0d358a3d
= <whyzoid@gmail.com>
2025-09-15 01:30:39 +0200

    Function to handle command-line arguments

--- mexec.c
+++ mexec.c
@@ -1,9 +1,50 @@
 #include <assert.h>
-#include <errno.h>
+// #include <errno.h>
 #include <stdbool.h>
 #include <stddef.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[]) { return EXIT_SUCCESS; }
+// Functions
+
+/*
+ * handleCmdLineArgs -
+ *
+ * @
+ * @
+ *
+ * Returns:
+ * */
+FILE *handleCmdLineArgs(int argc, char *argv[]);
+
+int main(int argc, char *argv[]) {
+
+  FILE *file = handleCmdLineArgs(argc, argv);
+
+  if (!file)
+    return EXIT_FAILURE;
+
+  return EXIT_SUCCESS;
+}
+
+FILE *handleCmdLineArgs(int argc, char *argv[]) {
+  if (argc > 2) {
+    fprintf(stderr, "Usage: %s [file]\n", argv[0]);
+
+    return NULL;
+  }
+
+  if (argc == 2) {
+    FILE *file = fopen(argv[1], "r");
+
+    if (!file) {
+      perror("Couldnt open the file");
+      return NULL;
+    }
+
+    return file;
+  }
+
+  return stdin;
+}
