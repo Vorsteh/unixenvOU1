0b5afc1aaaddfb59ae904c5b5caeba9dc271e587
Vorsteh <whyzoid@gmail.com>
2025-09-18 16:11:50 +0200

    All commented and done

--- mexec.c
+++ mexec.c
@@ -8,23 +8,93 @@
 // Functions
 
 /*
- * handleCmdLineArgs -
+ * handleCmdLineArgs - Makes sure program has correct amount of arguments
  *
- * @
- * @
+ * @param argc    number of command arguments
+ * @param *argv[] Aarray of command arugments
  *
- * Returns:
+ * Returns: FILE * to the opend file or stdin
+ *          Returns NULL on error
  * */
 FILE *handleCmdLineArgs(int argc, char *argv[]);
+
+/*
+ * parse_line - Splits lines into array of arguments
+ *
+ * @param buffer  String containting arguments
+ *
+ * Returns: Array of strings based on all arguments in main string
+ *
+ * */
+
 char **parse_line(char *buffer);
 
-// Free strings & commands allocated for arguments
+/*
+ * free_args - Frees memory allocated by malloc and realloc
+ *
+ * @param args Array of string argumetns
+ *
+ * @return void
+ */
 void free_args(char **args);
 
+/*
+ * commands_setup - Setups all commands from input file or stdin and stores them
+ * in an dynamically allocated array
+ *
+ * @param file              Input file/stdin
+ * @param commands_amount   Pointer to commands_amount that keeps track of
+ * amount of commands
+ *
+ * @return Dynamically allocated array of arguments array
+ */
+
 char ***commands_setup(FILE *file, int *commands_amount);
+
+/*
+ * pipes_setup - Setups pipes for communcation between processes through a
+ * pipeline
+ *
+ * @param commands_amount   The amount of commands
+ *
+ * @return 2D array of file descriptors
+ *
+ * */
 int **pipes_setup(int commands_amount);
+
+/*
+ * fork_setup - Forks a new process for each command and setups the pipeline by
+ * redirecting the child processes stdin and stdout
+ *
+ * @param commands          Array of commands
+ * @param commands_amount   Number of commands
+ * @param pipes             Array of pipes
+ *
+ * @return void
+ * */
 void fork_setup(char ***commands, int commands_amount, int **pipes);
+
+/*
+ * wait_for_children - Waits for child processes and checks exit status
+ *
+ * @param commands_amount  Number of commands
+ *
+ * @return void
+ *
+ * */
 void wait_for_children(int commands_amount);
+
+/*
+ * cleanup - Cleans up all dynamically allocated memory and closes all files and
+ * pipes
+ *
+ * @param file              Input file
+ * @param commands          Array of commands
+ * @param commands_amount   Amount of commands
+ * @param pipes             Array of pipes
+ *
+ * @reutnr void
+ * */
 void cleanup(FILE *file, char ***commands, int commands_amount, int **pipes);
 
 int main(int argc, char *argv[]) {
@@ -41,7 +111,6 @@ int main(int argc, char *argv[]) {
   fork_setup(commands, commands_amount, pipes);
   wait_for_children(commands_amount);
 
-  // CLEANUP
   cleanup(file, commands, commands_amount, pipes);
 
   return EXIT_SUCCESS;
@@ -49,12 +118,14 @@ int main(int argc, char *argv[]) {
 
 FILE *handleCmdLineArgs(int argc, char *argv[]) {
 
+  // More then two arguments print usage message
   if (argc > 2) {
     fprintf(stderr, "Usage: %s [file]\n", argv[0]);
 
     return NULL;
   }
 
+  // if correct amount we should open the file provided in read mode
   if (argc == 2) {
     FILE *file = fopen(argv[1], "r");
     if (!file) {
@@ -77,15 +148,18 @@ char **parse_line(char *buffer) {
     exit(EXIT_FAILURE);
   }
 
+  // split the buffern at spaces tabs and new lines
   char *tokens = strtok(buffer, " \t\n");
 
   int i = 0;
+  // Loop over and save duplicate each token to args
   while (tokens != NULL) {
     args[i] = strdup(tokens);
     i++;
     tokens = strtok(NULL, " \t\n");
   }
 
+  // put null at end for execvp
   args[i] = NULL;
   return args;
 }
@@ -104,8 +178,10 @@ char ***commands_setup(FILE *file, int *commands_amount) {
   int amount = 0;
   char ***commands = NULL;
 
+  // read entire file or stdin
   while (fgets(line, MAX_LINE, file)) {
 
+    // parse each line into array of arguments
     char **args = parse_line(line);
     if (!args)
       exit(EXIT_FAILURE);
@@ -115,16 +191,20 @@ char ***commands_setup(FILE *file, int *commands_amount) {
       continue;
     }
 
+    // resize commands to be able to store more args
     commands = realloc(commands, sizeof(char **) * (amount + 1));
 
+    // check realloc
     if (!commands) {
       perror("realloc");
       exit(EXIT_FAILURE);
     }
 
+    // save the args after reallocaing
     commands[amount] = args;
     amount++;
   }
+  // updates the commands_amount by pointer
   *commands_amount = amount;
 
   return commands;
@@ -133,6 +213,7 @@ char ***commands_setup(FILE *file, int *commands_amount) {
 int **pipes_setup(int commands_amount) {
   int pipes_amount = commands_amount - 1;
 
+  // Allocate memory for array of pipes
   int **pipes = malloc(sizeof(int *) * pipes_amount);
 
   if (!pipes) {
@@ -141,12 +222,14 @@ int **pipes_setup(int commands_amount) {
   }
 
   for (int i = 0; i < pipes_amount; i++) {
+    // allocate memory for pipe read and write ends
     pipes[i] = malloc(sizeof(int) * 2);
 
     if (!pipes[i]) {
       perror("malloc");
       exit(EXIT_FAILURE);
     }
+    // run pipe on each FD and check that it works
     if (pipe(pipes[i]) == -1) {
       perror("pipe");
       exit(EXIT_FAILURE);
@@ -158,30 +241,39 @@ int **pipes_setup(int commands_amount) {
 
 void fork_setup(char ***commands, int commands_amount, int **pipes) {
   int pipes_amount = commands_amount - 1;
+  // loop over all commands
   for (int i = 0; i < commands_amount; i++) {
+    // fork for each command
     int pid = fork();
     if (pid == -1) {
       perror("fork");
       exit(EXIT_FAILURE);
     }
 
+    // if we are in the child
+    // redirect the childs STDIN to the previous pipe
+    // redirect the child STDOUT to the current pipe, skips the last command so
+    // it actaully outputs something
     if (pid == 0) {
       if (i > 0)
         dup2(pipes[i - 1][0], STDIN_FILENO);
       if (i < commands_amount - 1)
         dup2(pipes[i][1], STDOUT_FILENO);
 
+      // close all origional pipes since dup2 duplicates them
       for (int j = 0; j < pipes_amount; j++) {
         close(pipes[j][0]);
         close(pipes[j][1]);
       }
 
+      // execute the commands
       execvp(commands[i][0], commands[i]);
       perror("execvp");
       exit(EXIT_FAILURE);
     }
   }
 
+  // close the parents copies of pipes
   for (int i = 0; i < pipes_amount; i++) {
     close(pipes[i][0]);
     close(pipes[i][1]);
@@ -190,8 +282,10 @@ void fork_setup(char ***commands, int commands_amount, int **pipes) {
 
 void wait_for_children(int commands_amount) {
   int status;
+  // waits for all children
   for (int i = 0; i < commands_amount; i++) {
     wait(&status);
+    // checks exitcodes for all children
     if (!WIFEXITED(status) || WEXITSTATUS(status) != 0) {
       exit(EXIT_FAILURE);
     }
@@ -199,6 +293,8 @@ void wait_for_children(int commands_amount) {
 }
 
 void cleanup(FILE *file, char ***commands, int commands_amount, int **pipes) {
+  // CLOSES AND CLEANUPS EVERYTHING
+
   if (file != stdin) {
     fclose(file);
   }
